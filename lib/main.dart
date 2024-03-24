499f7352736b73bc764ef5dd989fbcd731fe1680
mahin <mahincse47@gmail.com>
2024-03-25 05:25:07 +0600

    update for row column

--- lib/main.dart
+++ lib/main.dart
@@ -17,25 +17,8 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
       body:
       Center(
-        child:
-        // Text('Hello World!, Welcome to App!, Right now we are learing. Help me Allah!,'
-        //     'In Sha Allah',
-        // textAlign: TextAlign.center,
-        // maxLines: 2,
-        // style: TextStyle(
-        //   overflow: TextOverflow.ellipsis,
-        //   fontSize: 20,
-        //   fontWeight: FontWeight.bold,
-        //   fontStyle: FontStyle.italic,
-        //   backgroundColor: Colors.yellowAccent
-        // ),
-        // ),
-        //child: Image.network('https://www.pinterest.com/pin/773704411013971044/')
-      //  child: Image.asset('images/wallpepar.jpeg'),
-        Icon(Icons.android)
       ),
     );
   }

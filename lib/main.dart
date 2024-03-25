3ac5a29e67a51fad48649ae1bb229522e6cbbd1d
mahin <mahincse47@gmail.com>
2024-03-25 06:03:43 +0600

    update for row column

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// amra kiki dekbo ajke: column | row| dialog |bottom sheet
 import 'package:flutter/material.dart';
 
 void main(){
@@ -19,6 +20,26 @@ class Home extends StatelessWidget {
     return Scaffold(
       body:
       Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Hello Mahin!'),
+            Text('Welcome to your Flutter World.'),
+            Text('Lets Rock'),
+
+
+            Row(
+              children: [
+                Text('Greetings from'),
+                Text('Flutter'),
+              ],
+            )
+
+          ],
+        )
       ),
     );
   }

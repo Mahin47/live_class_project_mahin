60e59d7954a13f8d398b12029cfabd1a5ff703e0
mahin <mahincse47@gmail.com>
2024-04-02 02:47:47 +0600

    Module7class1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// Button( ElevatedButton, IconButton, TextButton, GestureDetector,InkWell | TextField
+// Container || RichText
 import 'package:flutter/material.dart';
 
 void main(){
@@ -17,25 +19,8 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
-      body:
-      Center(
-        child:
-        // Text('Hello World!, Welcome to App!, Right now we are learing. Help me Allah!,'
-        //     'In Sha Allah',
-        // textAlign: TextAlign.center,
-        // maxLines: 2,
-        // style: TextStyle(
-        //   overflow: TextOverflow.ellipsis,
-        //   fontSize: 20,
-        //   fontWeight: FontWeight.bold,
-        //   fontStyle: FontStyle.italic,
-        //   backgroundColor: Colors.yellowAccent
-        // ),
-        // ),
-        //child: Image.network('https://www.pinterest.com/pin/773704411013971044/')
-      //  child: Image.asset('images/wallpepar.jpeg'),
-        Icon(Icons.android)
+      appBar: AppBar(
+        
       ),
     );
   }

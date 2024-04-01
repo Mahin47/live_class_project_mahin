9dc6cf22f9c678e144dbc2e6c8db516cf57ae3b6
mahin <mahincse47@gmail.com>
2024-04-02 03:23:46 +0600

    Module7class1

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,84 @@
 // Container || RichText
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(Myapp());
 }
 
-class Myapp extends StatelessWidget{
+class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Hello World',
-        home: Home()
-    );
+    return MaterialApp(title: 'Hello World', home: Home());
   }
 }
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Row(
+            children: [
+              //Icon(Icons.home,color: Colors.white),
+              Text(
+                'Home Screen',
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+          leading: Icon(Icons.home_filled, color: Colors.white),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  // ScaffoldMessenger.of(context).showSnackBar(
+                  //     SnackBar(
+                  //         content: Text('You Have Been Pressed The Button'),
+                  //       duration: Duration(seconds: 1),
+                  //       backgroundColor: Colors.green
+                  //     )
+                  // );
+                  showDialog(
+                      context: context,
+                      builder: (Builder) {
+                        return AlertDialog(
+                          title: Text('Confirmation'),
+                          content: Text('Are You Want to Close this App?'),
+                        );
+                      });
+                },
+                icon: Icon(Icons.add)),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    side: BorderSide(
+                      width: 2,
+                      color: Colors.blueGrey
+                    )
+                  ),
+                  textStyle: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  ),
+                  padding: EdgeInsets.all(15)
+                ),
+                  onPressed: () {
+                    print('Button Pressed!');
+                  },
+                  child: Text('Press'))
+            ],
+          ),
+        ));
   }
-
-
 }

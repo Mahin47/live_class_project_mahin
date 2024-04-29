9caa4ee44b2b069406016c58f01765bc575a75a0
mahin <mahincse47@gmail.com>
2024-04-30 00:19:53 +0600

    Module 10, Exam 2

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Home(),
+        theme: ThemeData(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyanAccent,
+      ),
+    );
+  }
+}

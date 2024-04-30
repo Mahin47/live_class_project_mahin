fc82ba6823963b32d40d4f79bd50dfb09a07e5ae
mahin <mahincse47@gmail.com>
2024-04-30 23:42:58 +0600

    Module 10, Exam 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,73 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(Myapp());
+void main() {
+  runApp(MyApp());
 }
 
-class Myapp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Hello World',
-        home: Home()
+      title: 'Profile',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RoundImageScreen(),
     );
   }
 }
-class Home extends StatelessWidget {
+
+class RoundImageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
-      body:
-      Center(
-        child:
-        // Text('Hello World!, Welcome to App!, Right now we are learing. Help me Allah!,'
-        //     'In Sha Allah',
-        // textAlign: TextAlign.center,
-        // maxLines: 2,
-        // style: TextStyle(
-        //   overflow: TextOverflow.ellipsis,
-        //   fontSize: 20,
-        //   fontWeight: FontWeight.bold,
-        //   fontStyle: FontStyle.italic,
-        //   backgroundColor: Colors.yellowAccent
-        // ),
-        // ),
-        //child: Image.network('https://www.pinterest.com/pin/773704411013971044/')
-      //  child: Image.asset('images/wallpepar.jpeg'),
-        Icon(Icons.android)
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.9,
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 120.0,
+                backgroundImage: AssetImage('images/pic1.jpg'),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Mahadia Nur',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              //SizedBox(height: 8.0),
+              Text(
+                'Mahadia.nur@gmail.com',
+                style: TextStyle(
+                  fontSize: 14.0,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              SizedBox(height: 14),
+              Center(
+                child: Text(
+                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, '
+                      'when an unknown printer took a galley of type and scrambled it to make a type specimen book. '
+                      'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. '
+                      'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
-
-
 }
